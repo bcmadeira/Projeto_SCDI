3417ed7da6afb0fe161d1b97d0c50fc7f931b1b4
Bruno Madeira <brunomadeira044@gmail.com>
2025-10-08 15:01:20 -0300

    Criando Migration, Controller e Model de Doador

---
+++ app/Http/Controllers/DoadorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Doador;
+use Illuminate\Http\Request;
+
+class DoadorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Doador  $doador
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Doador $doador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Doador  $doador
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Doador $doador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Doador  $doador
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Doador $doador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Doador  $doador
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Doador $doador)
+    {
+        //
+    }
+}
