e4800cd6ee78ade6c80a1374f4b7dccf3cd2d6a4
Bruno Madeira <brunomadeira044@gmail.com>
2025-10-08 14:55:35 -0300

    Criando Migration,Controller e Model da Campanha

---
+++ app/Http/Controllers/CampanhaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Campanha;
+use Illuminate\Http\Request;
+
+class CampanhaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Campanha  $campanha
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Campanha $campanha)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Campanha  $campanha
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Campanha $campanha)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Campanha  $campanha
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Campanha $campanha)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Campanha  $campanha
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Campanha $campanha)
+    {
+        //
+    }
+}
