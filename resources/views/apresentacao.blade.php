1efb2ccbea04ba1d556f4f9a422b915b914bc7db
Leonardo <Leonardotuio@hotmail.com>
2025-10-15 09:40:56 -0300

    Tela de apresentação criada, imagens add, navbar css

--- resources/views/apresentacao.blade.php
+++ resources/views/apresentacao.blade.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema De Doações </title>
+    <title> Sistema de controle De Doações Institucional</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<body>
-    <h1>Sistema de doações</h1>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-3 mb-5 bg-body rounded py-3">
+  <div class="container">
+    <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+      <img src="{{ asset('images/projeto.png') }}" alt="Logo" width="80" height="80 " class="float-start me-3">
+      <span class="font-weight-light fs-5 text-dark "> Sistema De Controle De Doações Institucional</span>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item mx-2">
+          <a class="nav-link active nav-hover" href="{{ url('/') }}">Início</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link nav-hover" href="{{ url('/sobre') }}">Sobre</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link nav-hover" href="{{ url('/servicos') }}">Serviços</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link nav-hover" href="{{ url('/contato') }}">Contato</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
+
 </html>
