3d200918baa7678a65f03e81f3c9f6ac305a62ea
Bruno Madeira <brunomadeira044@gmail.com>
2025-10-19 15:33:16 -0300

    Adicionando Rota de Instituições

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('apresentacao');
 });
+
+Route::get('/instituicoes', function () {
+    return view('./Instituicoes/instituicoes');
+});
