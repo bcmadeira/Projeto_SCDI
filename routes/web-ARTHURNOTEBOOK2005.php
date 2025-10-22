67098e1477f9176408e2de2662f9b3fcc1672f93
MrIlustration2005 <arthurmrodrigues@gmail.com>
2025-10-22 14:21:42 -0300

    fazendotrabalho

--- routes/web-ARTHURNOTEBOOK2005.php
+++ routes/web-ARTHURNOTEBOOK2005.php
@@ -8,18 +8,11 @@ Route::get('/', function () {
     return view('apresentacao');
 });
 
-// Rota para teste do header
-Route::get('/teste', function () {
-    return view('teste');
-})->name('teste');
-
 Route::get('/campanhas', [CampanhaController::class, 'index'])->name('campanhas.index');
 Route::get('/campanhas/{id}', [CampanhaController::class, 'show'])->name('campanhas.show');
 
 Route::get('/campanhas/criar', [CampanhaController::class, 'create'])->name('campanhas.create');
 Route::post('/campanhas', [CampanhaController::class, 'store'])->name('campanhas.store');
 
-Route::get('/relatorio', function () {
-    return view('Adm.relatorio'); // note que aqui é Adm.relatorio (sem .blade.php)
-});
+
 
